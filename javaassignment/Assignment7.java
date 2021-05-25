78718b59ddf70b2814e4a62ccbb0972257c8ce77
mg143pavankumar <pavansharma.mg143@gmail.com>
2021-05-25 07:09:02 +0530

    Added assignment7 which is on inheritance

---
+++ javaassignment/Assignment7.java
@@ -0,0 +1,41 @@
+package com.company;
+
+class OTT
+{
+    public static void ottFeatures()
+    {
+        System.out.println("Features of OTT:\n"
+                + "1.ad free\n"
+                +"2.live streaming\n"
+                +"3.ease to use\n"
+                +"4.subtitle\n");
+    }
+}
+
+class AmazonPrimeVideo extends OTT
+{
+    public static void display()
+    {
+        System.out.println("Movie name:The Family Man 2\n"
+                +"Release Date:june 4,2021\n\n");
+    }
+}
+
+class ZEE5 extends OTT
+{
+    public static void display()
+    {
+        System.out.println("Movie name:Aranya\n"
+                +"Release Date:june 4,2021\n");
+    }
+}
+
+public class Assignment7 {
+    public static void main(String[] args) {
+        AmazonPrimeVideo.ottFeatures();
+        AmazonPrimeVideo.display();
+
+        ZEE5.ottFeatures();
+        ZEE5.display();
+    }
+}
